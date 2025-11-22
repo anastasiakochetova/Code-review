9fa4ba08d7249e9f39bda553bf48907db69102fd
Кочетова Анастасия <132236anastasiakoc@users.no-reply.gitverse.ru>
2025-11-22 10:56:32 +0000

    update assignment.py

--- assignment.py
+++ assignment.py
@@ -13,7 +13,6 @@
 8. Визуализировать категориальные признаки в visualize_categorical_features()
 9. Анализировать выживаемость по признакам в survival_by_features()
 """
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -26,72 +25,113 @@ plt.rcParams['axes.unicode_minus'] = False
 
 def load_data():
     """Загрузить датасет Titanic с Hugging Face"""
-    # TODO: Загрузить датасет "mstz/titanic"
-    # TODO: Конвертировать в pandas DataFrame
-    pass
+    dataset = load_dataset("mstz/titanic")
+    df = pd.DataFrame(dataset['train'])  # используем train split
+    return df
 
 
 def missing_analysis(df):
     """Анализ пропущенных значений"""
-    # TODO: Найти колонки с пропущенными значениями
-    # TODO: Вычислить процент пропусков
-    # TODO: Вывести результаты
-    pass
+    missing_count = df.isnull().sum()
+    missing_percent = (missing_count / len(df)) * 100
+    missing_df = pd.DataFrame({
+        'missing_count': missing_count,
+        'missing_percent': missing_percent
+    })
+    missing_df = missing_df[missing_df['missing_count'] > 0].sort_values(by='missing_percent', ascending=False)
+    print("\nПропущенные значения по колонкам:")
+    print(missing_df)
 
 
 def target_analysis(df):
     """Анализ целевой переменной (выживаемость)"""
-    # TODO: Вывести распределение выживания (counts)
-    # TODO: Вывести процент умерших и выживших
-    pass
+    counts = df['survived'].value_counts()
+    print("\nРАСПРЕДЕЛЕНИЕ ВЫЖИВШИХ/ПАССАЖИРОВ:")
+    print(counts)
+    print("\nПРОЦЕНТНОЕ РАСПРЕДЕЛЕНИЕ:")
+    print(counts / counts.sum() * 100)
 
 
 def feature_statistics(df):
     """Вычислить статистику по числовым признакам"""
-    # TODO: Для каждого числового признака вычислить:
-    # - Среднее, медиана, стандартное отклонение
-    # - Минимум, максимум
-    pass
+    numeric_cols = df.select_dtypes(include=[np.number]).columns
+    stats = df[numeric_cols].describe().T[['mean', '50%', 'std', 'min', 'max']]
+    stats.rename(columns={'50%': 'median'}, inplace=True)
+    print("\nСтатистика числовых признаков:")
+    print(stats)
 
 
 def categorical_analysis(df):
     """Анализ категориальных признаков"""
-    # TODO: Анализировать распределение Sex, Pclass, Embarked
-    # TODO: Вывести value_counts для каждого признака
-    pass
+    categorical_cols = ['sex', 'pclass', 'embarked']
+    for col in categorical_cols:
+        print(f"\nРаспределение {col}:")
+        print(df[col].value_counts())
 
 
 def visualize_target(df):
     """Визуализировать распределение целевой переменной"""
-    # TODO: Создать столбчатую диаграмму выживания
-    # TODO: Создать круговую диаграмму с процентами
-    # TODO: Сохранить как 06_titanic_target_distribution.png
-    pass
+    plt.figure(figsize=(10,4))
+
+    # Столбчатая диаграмма
+    plt.subplot(1,2,1)
+    sns.countplot(x='survived', data=df)
+    plt.title('Survival Count')
+
+    # Круговая диаграмма
+    plt.subplot(1,2,2)
+    df['survived'].value_counts().plot.pie(autopct='%1.1f%%', labels=['Died', 'Survived'])
+    plt.title('Survival Percentage')
+    plt.ylabel('')
+
+    plt.tight_layout()
+    plt.savefig("06_titanic_target_distribution.png")
+    plt.show()
 
 
 def visualize_numeric_features(df):
     """Визуализировать числовые признаки"""
-    # TODO: Для каждого числового признака создать гистограмму
-    # TODO: Расположить в сетке 2x2
-    # TODO: Сохранить как 06_titanic_numeric_distribution.png
-    pass
+    numeric_cols = df.select_dtypes(include=[np.number]).columns
+    plt.figure(figsize=(10,8))
+    for i, col in enumerate(numeric_cols):
+        plt.subplot(2,2,i+1)
+        sns.histplot(df[col].dropna(), kde=True, bins=30)
+        plt.title(col)
+    plt.tight_layout()
+    plt.savefig("06_titanic_numeric_distribution.png")
+    plt.show()
 
 
 def visualize_categorical_features(df):
     """Визуализировать категориальные признаки"""
-    # TODO: Для Sex, Pclass, Embarked создать столбчатые диаграммы
-    # TODO: Расположить в ряд из 3 графиков
-    # TODO: Сохранить как 06_titanic_categorical_distribution.png
-    pass
+    categorical_cols = ['sex', 'pclass', 'embarked']
+    plt.figure(figsize=(15,4))
+    for i, col in enumerate(categorical_cols):
+        plt.subplot(1,3,i+1)
+        sns.countplot(x=col, data=df)
+        plt.title(col)
+    plt.tight_layout()
+    plt.savefig("06_titanic_categorical_distribution.png")
+    plt.show()
 
 
 def survival_by_features(df):
     """Анализ выживаемости по разным признакам"""
-    # TODO: Создать диаграммы выживаемости по полу (Sex)
-    # TODO: Создать диаграммы выживаемости по классу билета (Pclass)
-    # TODO: Расположить в ряд из 2 графиков
-    # TODO: Сохранить как 06_titanic_survival_by_features.png
-    pass
+    plt.figure(figsize=(12,5))
+
+    # По полу
+    plt.subplot(1,2,1)
+    sns.barplot(x='sex', y='survived', data=df)
+    plt.title('Survival by Sex')
+
+    # По классу
+    plt.subplot(1,2,2)
+    sns.barplot(x='pclass', y='survived', data=df)
+    plt.title('Survival by Pclass')
+
+    plt.tight_layout()
+    plt.savefig("06_titanic_survival_by_features.png")
+    plt.show()
 
 
 def main():
@@ -122,5 +162,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# Последнее обновление: 2025-11-21 04:59:15
